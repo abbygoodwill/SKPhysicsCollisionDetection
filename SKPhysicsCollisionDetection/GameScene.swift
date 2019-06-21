49db9bbe3703708ab53bfc66dc11135a0d3015d3
Abby Goodwill <abbygoodwill@Jamess-iMac.local>
2019-06-21 16:53:31 -0600

    didBeginContact

--- SKPhysicsCollisionDetection/GameScene.swift
+++ SKPhysicsCollisionDetection/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "player")
     let coin = SKSpriteNode(imageNamed: "coin")
+    let CollisionCategoryPlayer: UInt32 = 0x1 << 1
+    let CollisionCategoryCoin: UInt32 = 0x1 << 2
     
     // required init
     required init?(coder aDecoder: NSCoder) {
@@ -46,12 +48,17 @@ class GameScene: SKScene {
         player.physicsBody?.isDynamic = true
         player.physicsBody?.affectedByGravity = false
         player.physicsBody?.allowsRotation = false
+        player.physicsBody?.categoryBitMask = CollisionCategoryPlayer
+        player.physicsBody?.contactTestBitMask = CollisionCategoryCoin
+        player.physicsBody?.collisionBitMask = 0
         addChild(player)
         
         coin.position = CGPoint(x: size.width / 4.0, y: 450.0)
         coin.physicsBody = SKPhysicsBody(circleOfRadius: coin.size.width / 2.0)
         coin.physicsBody?.isDynamic = true
         coin.physicsBody?.affectedByGravity = true
+        coin.physicsBody?.categoryBitMask = CollisionCategoryCoin
+        coin.physicsBody?.collisionBitMask = 0
         addChild(coin)
     }
     
